81fe0bb7dae41c501f7ad89d9f600b9697f5ba9f
EdwinCh <eachitanc@gmail.com>
2020-12-30 10:33:37 -0500

    ajustes color

--- resources/views/encabezado.blade.php
+++ resources/views/encabezado.blade.php
@@ -1,5 +1,5 @@
 <!--Navbar -->
-<nav id="barraPrincipal" class="mb-1 navbar navbar-expand-lg navbar-light">
+<nav id="barraPrincipal" class="mb-1 navbar navbar-expand-lg navbar-dark">
     <a class="navbar-brand" href="#">LA UNICA</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
         aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,51 +7,42 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
         <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Inicio
-            <span class="sr-only">(current)</span>
-            </a>
-        </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Inicio
+                    <span class="sr-only">(current)</span>
+                </a>
+            </li>
         <li class="nav-item">
             <a class="nav-link" href="#">Ofertas</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Pricing</a>
+            <a class="nav-link" href="#">Gestion</a>
         </li>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">Dropdown
+            aria-haspopup="true" aria-expanded="false">Productos
             </a>
             <div class="dropdown-menu dropdown-default" aria-labelledby="navbarDropdownMenuLink-333">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
+                <a class="dropdown-item" href="#">Cumpleaños</a>
+                <a class="dropdown-item" href="#">Aniversario</a>
+                <a class="dropdown-item" href="#">Amor y Amistad</a>
+                <a class="dropdown-item" href="#">Otros</a>
             </div>
         </li>
         </ul>
         <ul class="navbar-nav ml-auto nav-flex-icons">
-        <li class="nav-item">
-            <a class="nav-link waves-effect waves-light">
-            <i class="fab fa-twitter"></i>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link waves-effect waves-light">
-            <i class="fab fa-google-plus-g"></i>
-            </a>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-user"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right dropdown-default"
-            aria-labelledby="navbarDropdownMenuLink-333">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-        </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-333" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-user"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-default"
+                aria-labelledby="navbarDropdownMenuLink-333">
+                    <a class="dropdown-item" href="#">Iniciar Sesión</a>
+                    <a class="dropdown-item" href="#">Actializar Perfíl</a>
+                    <a class="dropdown-item" href="#">Cerrar Sesión</a>
+                </div>
+            </li>
         </ul>
     </div>
 </nav>
