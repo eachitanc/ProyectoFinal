920cfa812fd1f5c30e03ee9e2d22adfd6353d5f7
EdwinCh <eachitanc@gmail.com>
2021-01-07 13:22:39 -0500

    model- registro

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Usuario;
 
 class LoginController extends Controller
 {
@@ -14,15 +15,25 @@ class LoginController extends Controller
             return view('login.login');
         }else{
             $user  = DB::table('usuario')
-                    ->where('user','=',$us)
+                    ->where('user','=',"$us")
                     ->get();
             return view('logeado.inicio', ['user' => $user]);
         }
         
     }
 
-    public function registroUser(Request $request){        
+    public function registroUser(Request $request){   
+        $us = new Usuario();
+        $us -> user = $request->input('txtUser');
+        $us -> pass_user = $request->input('passUser');
+        $us -> nom_user = $request->input('txtNomUser');
+        $us -> ape_user = $request->input('txtApeUser');
+        $us -> correo_user = $request->input('emailUser');
+        $us -> rol = $request->input('numRol');
+        $us -> save(); 
+        return redirect () -> route ('formLogin');    
     }
+
 }
         
         
--- resources/views/login/registro.blade.php
+++ resources/views/login/registro.blade.php
@@ -10,20 +10,22 @@
                 </div>
                 <!-- Default form register -->
                 <form class="text-center border border-light p-5" action="{{ route('registroUsuario')}}" method="POST">
+                    @csrf
                     <div class="form-row mb-4">
                         <div class="col">
-                            <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="Nombre">
+                            <input type="text" id="txtNomUser" name="txtNomUser" class="form-control" placeholder="Nombre">
                         </div>
                         <div class="col">
-                            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Apellido">
+                            <input type="text" id="txtApeUser" name="txtApeUser" class="form-control" placeholder="Apellido">
                         </div>
                     </div>
-                    <input type="email" id="formRegCorreo" class="form-control mb-4" placeholder="Correo electrónico" required>
-                    <input type="text" id="formRegUser" class="form-control mb-4" placeholder="Usuario" required>
-                    <input type="password" id="formRegPass" class="form-control mb-4" placeholder="Contraseña" required>
+                    <input type="number" id="numRol" name="numRol" value="3" hidden >
+                    <input type="email" id="emailUser" name="emailUser" class="form-control mb-4" placeholder="Correo electrónico" required>
+                    <input type="text" id="txtUser" name="txtUser"class="form-control mb-4" placeholder="Usuario" required>
+                    <input type="password" id="passUser" name="passUser" class="form-control mb-4" placeholder="Contraseña" required>
 
                     <!-- Sign up button -->
-                    <a type="submit" class="btn btn-primary">Registrar <i class="far fa-user ml-1 text-white"></i></a>
+                    <button type="submit" class="btn btn-primary">Registrar <i class="far fa-user ml-1 text-white"></i></button>
                 </form>
                 <!-- Default form register -->
             </div>
